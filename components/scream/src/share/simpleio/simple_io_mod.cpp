6a311c9481214b2e7c2b9b82ab526dc34ada8e26
Aaron Donahue <donahue.aaron@gmail.com>
2019-09-03 12:18:45 -0700

    Fully implemented simple io for scream-ad.
    This is a basic implementation with a basic test.  Future work
    can make it more general and versatile.  But this should be good
    enough for now.

--- components/scream/src/share/simpleio/simple_io_mod.cpp
+++ components/scream/src/share/simpleio/simple_io_mod.cpp
@@ -12,13 +12,26 @@ extern "C" {
   void simple_out_init2();
   void simple_out_finalize();
   void simple_out_regfield(char** field_name,Int field_type,Int ndim,std::string (&field_dim)[ndim],char** units);
-  //void simple_out_writefield(char** field_name, Real* field_data);
-  void io_writefield_1d_real(char** field_name, Int fsize, Real* field_data);
+  void io_writefield_1d_real(char** field_name, Int time_dim, Int data_dim,  Real field_data[data_dim]);
+  void io_writefield_2d_real(char** field_name, Int time_dim, Int* data_dim, Real field_data[data_dim[0]][data_dim[1]]);
+  void io_writefield_3d_real(char** field_name, Int time_dim, Int* data_dim, Real field_data[data_dim[0]][data_dim[1]][data_dim[2]]);
+  void simple_in_init(char** field_name);
+  void simple_in_finalize();
+  void simple_io_get_field_size(char** field_name, Int ndims, Int* ldims);
+  void io_readfield_real(char** field_name, Int time_dim[2], Int data_dim,  Real field_data[data_dim]);
+  void io_readfield_1d_real(char** field_name, Int time_dim, Int data_dim,  Real field_data[data_dim]);
+  //void io_readfield_2d_real(char** field_name, Int time_dim, Int* data_dim, Real field_data[data_dim[0]][data_dim[1]]);
+  void io_readfield_2d_real(char** field_name, Int time_dim, Int* data_dim, Real** field_data);
+  void io_readfield_3d_real(char** field_name, Int time_dim, Int* data_dim, Real*** field_data);
+ // void io_readfield_3d_real(char** field_name, Int time_dim, Int* data_dim, Real field_data[data_dim[0]][data_dim[1]][data_dim[2]]);
 
 } // extern C
 
 namespace scream {
 namespace simpleio {
+  typedef std::vector<double> dbl_vector;
+  typedef std::vector<dbl_vector> dbl_2dmatrix;
+  typedef std::vector<dbl_2dmatrix> dbl_3dmatrix;
 /* ----------------------------------------------------------------- */
 int hello_world(int myint, double myreal, char** mychar,int len_planet, 
     std::string (&planets)[len_planet]) 
@@ -32,6 +45,121 @@ int init_output1(char** filename, int ndims, std::string (&dimnames)[ndims],
     int* dimrng)
 {
   simple_out_init1(filename,ndims,dimnames,dimrng);
+
+  return 0;
+}
+/* ----------------------------------------------------------------- */
+/* Overload read field function to handle different array sizes     */
+/* ----------------------------------------------------------------- */
+//int readfield(char** field_name, int time_dim, dbl_vector& res)
+//{
+//
+//  int ndims = 1;
+//  int len[1];
+//  simple_io_get_field_size(field_name,ndims,len);
+//
+//  double *field_data = NULL;
+//  field_data = new double[len[0]];
+//  io_readfield_1d_real(field_name,time_dim,len[0],field_data);
+//
+//  for (int i=0; i<len[0]; i++) {
+//    res.push_back( field_data[i] );
+//  }
+//  return 0;
+//}
+/* ----------------------------------------------------------------- */
+int readfield(char** field_name, int time_dim, dbl_3dmatrix& res)
+{
+
+  int ndims = 4;
+  int len[]={1,1,1,1};
+  int time_in[] = {1,1};
+  simple_io_get_field_size(field_name,ndims,len);
+  if (len[ndims-1]>1) {
+    time_in[0] = ndims;
+    time_in[1] = time_dim;
+  }
+  int flen = 1;
+  for (const auto& e:len) {
+    flen *=e;
+  }
+  double field_data[flen];
+
+  io_readfield_real(field_name,time_in,flen,field_data);
+
+  int ind = 0;
+  for (int i=0; i<len[2]; i++) {
+    dbl_2dmatrix mat;
+    for (int j=0; j<len[1]; j++) {
+      dbl_vector row;
+      for (int k=0; k<len[0]; k++) {
+        row.push_back( field_data[ind] );
+        ind +=1;
+      }
+      mat.push_back(row);
+    }
+    res.push_back( mat );
+  }
+
+  return 0;
+}
+/* ----------------------------------------------------------------- */
+int readfield(char** field_name, int time_dim, dbl_2dmatrix& res)
+{
+
+  int ndims = 3;
+  int len[]={1,1,1};
+  int time_in[] = {1,1};
+  simple_io_get_field_size(field_name,ndims,len);
+  if (len[ndims-1]>1) {
+    time_in[0] = ndims;
+    time_in[1] = time_dim;
+  }
+  int flen = 1;
+  for (const auto& e:len) {
+    flen *=e;
+  }
+  double field_data[flen];
+
+  io_readfield_real(field_name,time_in,flen,field_data);
+
+  int ind = 0;
+  for (int j=0; j<len[1]; j++) {
+    dbl_vector row;
+    for (int k=0; k<len[0]; k++) {
+      row.push_back( field_data[ind] );
+      ind +=1;
+    }
+    res.push_back(row);
+  }
+
+  return 0;
+}
+/* ----------------------------------------------------------------- */
+int readfield(char** field_name, int time_dim, dbl_vector& res)
+{
+
+  int ndims = 2;
+  int len[]={1,1};
+  int time_in[] = {1,1};
+  simple_io_get_field_size(field_name,ndims,len);
+  if (len[ndims-1]>1) {
+    time_in[0] = ndims;
+    time_in[1] = time_dim;
+  }
+  int flen = 1;
+  for (const auto& e:len) {
+    flen *=e;
+  }
+  double field_data[flen];
+
+  io_readfield_real(field_name,time_in,flen,field_data);
+
+  int ind = 0;
+  for (int k=0; k<len[0]; k++) {
+    res.push_back( field_data[ind] );
+    ind +=1;
+  }
 
   return 0;
 }
@@ -50,9 +178,47 @@ int finalize_output()
   return 0;
 }
 /* ----------------------------------------------------------------- */
+int init_input(char** filename)
+{
+  simple_in_init(filename);
+
+  return 0;
+}
+/* ----------------------------------------------------------------- */
+int finalize_input()
+{
+  simple_in_finalize();
+
+  return 0;
+}
+/* ----------------------------------------------------------------- */
 int regfield(char** field_name,int field_type,int ndim,std::string (&field_dim)[ndim],char** units)
 {
   simple_out_regfield(field_name,field_type,ndim,field_dim,units);
+
+  return 0;
+}
+/* ----------------------------------------------------------------- */
+/* Overload write field function to handle different array sizes     */
+/* ----------------------------------------------------------------- */
+int writefield(char** field_name, int time_dim, int data_dim, double* field_data)
+{
+
+ io_writefield_1d_real(field_name,time_dim,data_dim,field_data); 
+
+  return 0;
+}
+/* ----------------------------------------------------------------- */
+int writefield(char** field_name, int time_dim, int* data_dim, double field_data[data_dim[0]][data_dim[1]])
+{
+  io_writefield_2d_real(field_name,time_dim,data_dim,field_data); 
+
+  return 0;
+}
+/* ----------------------------------------------------------------- */
+int writefield(char** field_name, int time_dim, int* data_dim, double field_data[data_dim[0]][data_dim[1]][data_dim[2]])
+{
+  io_writefield_3d_real(field_name,time_dim,data_dim,field_data); 
 
   return 0;
 }
--- components/scream/src/share/tests/simple_io_tests.cpp
+++ components/scream/src/share/tests/simple_io_tests.cpp
@@ -8,8 +8,9 @@
 
 namespace {
 
-TEST_CASE("simple_io_mod", "test_simple_io") {
+TEST_CASE("simple_out_mod", "test_simple_out") {
 
+  SECTION("Hello World") {
 /* TODO: Delete
  * Simple hello_world to test input/output and c-binding
  */
@@ -19,24 +20,30 @@ TEST_CASE("simple_io_mod", "test_simple_io") {
   std::string planets[] = {"Mercury", "Venus", "Earth", "Mars", "Saturn", "Jupiter", "Neptune", "Uranus"};
   const int len_planet = sizeof(planets)/sizeof(planets[0]);
 
+  // Simple Hello World - TODO delete
+  int nerr = scream::simpleio::hello_world(myint, myreal, &mychar,len_planet,planets);
+  REQUIRE (nerr==0);
+  } // Hello world
+  SECTION("Test Output") {
 /* ====================================================================
  * Create sample output for simple_io test 
  * ==================================================================== */
   // Create sample output:
-  double lats[6], lons[12];
-  double pres[12][6][2], temp[12][6][2];
+  double lats[6]={0,0,0,0,0,0};
+  double lons[12];
+  double pres[2][6][12], temp[2][6][12];
   for (int n=0; n<6; n++) {
-    lats[n] = 25.0 + (n-1) * 5.0;
+    lats[n] = 25.0 + (n) * 5.0;
   }
   for (int n=0; n<12; n++) {
-    lons[n] = -125.0 + (n-1) * 5.0;
+    lons[n] = -125.0 + (n) * 5.0;
   }
   int n = 0;
   for (int k=0; k<2;k++) {
     for (int i=0; i<6; i++) {
       for (int j=0; j<12; j++) {
-        pres[j][i][k] = 900.0 + float(n);
-        temp[j][i][k] = 9.0   + float(n);
+        pres[k][i][j] = 900.0 + float(n);
+        temp[k][i][j] = 9.0   + float(n);
         n = n+1;
       }
     }
@@ -51,11 +58,8 @@ TEST_CASE("simple_io_mod", "test_simple_io") {
   char* filename = "pres_temp_4D.nc";
   std::string dimnames[] = {"longitude","latitude","level","time"};
   const int ndims = sizeof(dimnames)/sizeof(dimnames[0]);
-  int  dimrng[] = {12,6,2,0};
-
-  // Simple Hello World - TODO delete
-  int nerr = scream::simpleio::hello_world(myint, myreal, &mychar,len_planet,planets);
-  REQUIRE (nerr==0);
+  int dimrng[] = {12,6,2,0};
+  int dim3d[]  = {12,6,2};
 
   // Create the netcdf file
   int init1_err = scream::simpleio::init_output1(&filename, ndims, dimnames, dimrng);
@@ -63,7 +67,7 @@ TEST_CASE("simple_io_mod", "test_simple_io") {
   // Register all of the fields in the file
   int reg = 0;
   char *fieldname, *units;
-  int ftype, fdims, reg1;
+  int ftype, reg1;
   std::string vec_lat[] = {"latitude"};
   std::string vec_lon[] = {"longitude"};
   std::string vec_3d_state[] = {"longitude","latitude","level","time"};
@@ -103,6 +107,29 @@ TEST_CASE("simple_io_mod", "test_simple_io") {
 /* ====================================================================
  * Write sample output to netCDF output file.
  * ==================================================================== */
+ int wrt=0, wrt1;
+ int timelev = 1;
+ fieldname = "latitude";
+ wrt1 = scream::simpleio::writefield(&fieldname,timelev,dimrng[1],lats);
+ wrt = std::max(wrt,wrt1);
+ fieldname = "longitude";
+ wrt1 = scream::simpleio::writefield(&fieldname,timelev,dimrng[0],lons);
+ wrt = std::max(wrt,wrt1);
+ fieldname = "pressure";
+ wrt1 = scream::simpleio::writefield(&fieldname,timelev,dim3d,pres);
+ wrt = std::max(wrt,wrt1);
+ fieldname = "temperature";
+ wrt1 = scream::simpleio::writefield(&fieldname,timelev,dim3d,temp);
+ wrt = std::max(wrt,wrt1);
+ timelev++;
+ fieldname = "pressure";
+ wrt1 = scream::simpleio::writefield(&fieldname,timelev,dim3d,pres);
+ wrt = std::max(wrt,wrt1);
+ fieldname = "temperature";
+ wrt1 = scream::simpleio::writefield(&fieldname,timelev,dim3d,temp);
+ wrt = std::max(wrt,wrt1);
+
+ REQUIRE(wrt==0);
 
 /* ====================================================================
  * Finished with test, close netCDF file
@@ -110,6 +137,48 @@ TEST_CASE("simple_io_mod", "test_simple_io") {
   // All done, not close the file
   int final_err = scream::simpleio::finalize_output();
   REQUIRE (final_err==0);
+  } // Test output
+/* ====================================================================
+ * Test Input
+ * ==================================================================== */
+  SECTION("Test Input") {
+
+  // Open the test file
+  typedef std::vector<double> dbl_vector;
+  typedef std::vector<dbl_vector> dbl_2dmatrix;
+  typedef std::vector<dbl_2dmatrix> dbl_3dmatrix;
+  char* filename = "pres_temp_4D.nc";
+  char* fieldname;
+  int rd;
+
+  rd = scream::simpleio::init_input(&filename);
+
+  int time_dim = 1;
+
+  fieldname  = "latitude";
+  dbl_vector lat;
+  rd = scream::simpleio::readfield(&fieldname,time_dim,lat);
+  for (double n : lat){
+    std::cout << n << "\n";
+  }
+
+  fieldname = "pressure";
+  dbl_3dmatrix pres;
+  rd = scream::simpleio::readfield(&fieldname,time_dim,pres);
+  for (dbl_2dmatrix n1 : pres){
+    for (dbl_vector n2 : n1) {
+      for (double n3 : n2) {
+        std::cout << n3 << " ";
+      }
+      std::cout << "\n";
+    }
+    std::cout << "\n";
+  }
+
+  rd = scream::simpleio::finalize_input();
+  REQUIRE(rd==0);
+
+  } // Test Input
 
 } // TEST_CASE
 } // empty namespace
