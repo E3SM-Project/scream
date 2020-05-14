d0f17e9dba7b1fe28c08f936a5d952ef1c3e9062
Peter Bogenschutz <bogenschutz1@llnlg.ov>
2020-05-14 11:27:00 -0700

    initialize aist in shoc_intr for safety purposes

--- components/cam/src/physics/cam/shoc_intr.F90
+++ components/cam/src/physics/cam/shoc_intr.F90
@@ -318,6 +318,7 @@ end function shoc_implements_cnst
       call pbuf_set_field(pbuf2d, fice_idx, 0.0_r8)
       call pbuf_set_field(pbuf2d, tke_idx, tke_tol)
       call pbuf_set_field(pbuf2d, alst_idx, 0.0_r8)
+      call pbuf_set_field(pbuf2d, aist_idx, 0.0_r8)
       
       call pbuf_set_field(pbuf2d, vmag_gust_idx,    1.0_r8)
       
