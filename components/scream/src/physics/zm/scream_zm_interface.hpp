21b632f917ee253880f4098f2d2a16bf68e53ce4
Meredith Fossitt <fossitt1@llnl.gov>
2020-07-24 14:14:33 -0700

    zm_convr is only test not passing

--- components/scream/src/physics/zm/scream_zm_interface.hpp
+++ components/scream/src/physics/zm/scream_zm_interface.hpp
@@ -14,18 +14,6 @@ extern "C"
 
 // Fortran routines to be called from C
 void zm_init_f90     (const Real& limcnv_in, const bool& no_deep_pbl_in);
-//void zm_main_f90     (const Real& lchnk, const Real& ncol, Real* t, Real* qh, Real* prec,
-//			Real* jctop, Real* jcbot, Real* pblh, Real *zm, Real* geos, Real* zi,
-//			Real* qtnd, Real* heat, Real* pap, Real* paph, Real* dpp, const Real &delt,
-//			Real* mcon, Real* cme, Real* cape, Real* tpert, Real* dlf, Real* plfx,
-//			Real* zdu, Real* rprd, Real* mu, Real* md, Real* du, Real* eu, 
-//			Real* ed, Real* dp, Real* dsubcld, Real* jt, Real* maxg, Real* ideep,
-//			const Real& lengath, Real* ql, Real* rliq, Real* landfrac, Real* hu_nm1,
-//			Real* cnv_nm1, Real* tm1, Real* qm1, Real** t_star, Real** q_star,
-//			Real *dcape, Real* q, Real** tend_s, Real** tend_q, Real** cld, 
-//			Real* snow, Real* ntprprd, Real* ntsnprd, Real** flxprec, Real** flxsnow,
-//			const Real& ztodt, Real* pguall, Real* pgdall, Real* icwu, const Real& ncnst );
-//
 void zm_main_f90(const Real& lchnk, const Real& ncol, Real* t, Real* qh, Real* prec,
 			Real* jctop, Real* jcbot, Real* pblh, Real *zm, Real* geos, Real* zi,
 			Real* qtnd, Real* heat, Real* pap, Real* paph, Real* dpp, const Real &delt,
@@ -36,8 +24,8 @@ void zm_main_f90(const Real& lchnk, const Real& ncol, Real* t, Real* qh, Real* p
 			Real* cnv_nm1, Real* tm1, Real* qm1, Real** t_star, Real** q_star, 
 			Real* dcape, Real* q, Real** tend_s,
 			Real** tend_q, Real** cld, Real* snow, Real* ntprprd, Real* ntsnprd,
-			Real** flxprec, Real** flxsnow 
-			//Real* tm1, Real* qm1, Real** t_star, Real** q_star
+			Real** flxprec, Real** flxsnow, const Real& ztodt, Real* pguall, Real* pgdall, 
+			Real* icwu, const Real& ncnst 
 			); 
 //			Real* snow, Real* ntprprd, Real* ntsnprd, Real** flxprec, Real** flxsnow,
 //			const Real& ztodt, Real* pguall, Real* pgdall, Real* icwu, const Real& ncnst);
