699bcb1fe82e6f7968070d5fa2995b303b1d844b
Meredith Fossitt <fossitt1@llnl.gov>
2020-07-24 14:02:10 -0700

    Changed dynamic fortran pointers

--- components/scream/src/physics/zm/scream_zm_interface.hpp
+++ components/scream/src/physics/zm/scream_zm_interface.hpp
@@ -32,8 +32,12 @@ void zm_main_f90(const Real& lchnk, const Real& ncol, Real* t, Real* qh, Real* p
 			Real* mcon, Real* cme, Real* cape, Real* tpert, Real* dlf, Real* plfx,
 			Real* zdu, Real* rprd, Real* mu, Real* md, Real* du, Real* eu, 
 			Real* ed, Real* dp, Real* dsubcld, Real* jt, Real* maxg, Real* ideep,
-			const Real& lengath, Real* ql, Real* rliq, Real* landfrac, Real* hu_nm1
-			//Real* cnv_nm1, Real* tm1, Real* qm1, Real** t_star, Real** q_star
+			const Real& lengath, Real* ql, Real* rliq, Real* landfrac, Real* hu_nm1,
+			Real* cnv_nm1, Real* tm1, Real* qm1, Real** t_star, Real** q_star, 
+			Real* dcape, Real* q, Real** tend_s,
+			Real** tend_q, Real** cld, Real* snow, Real* ntprprd, Real* ntsnprd,
+			Real** flxprec, Real** flxsnow 
+			//Real* tm1, Real* qm1, Real** t_star, Real** q_star
 			); 
 //			Real* snow, Real* ntprprd, Real* ntsnprd, Real** flxprec, Real** flxsnow,
 //			const Real& ztodt, Real* pguall, Real* pgdall, Real* icwu, const Real& ncnst);
