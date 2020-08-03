06d1b09e85afc7da42b687a57a2fb31bd0e12446
Meredith Fossitt <fossitt1@llnl.gov>
2020-08-03 12:11:09 -0700

    making add fields funcs more dynamic

--- components/scream/src/physics/zm/atmosphere_macrophysics.cpp
+++ components/scream/src/physics/zm/atmosphere_macrophysics.cpp
@@ -1,13 +1,22 @@
 #include "ekat/scream_assert.hpp"
-#include "physics/zm/zm_inputs_initializer.hpp"
 #include "physics/zm/scream_zm_interface.hpp"
 #include "physics/zm/atmosphere_macrophysics.hpp"
+#include "physics/zm/zm_inputs_initializer.cpp"
 #include <iostream>
 namespace scream
 
 {
-std::vector<std::string> zm_inputs = {"t"};
-
+//std::vector<std::string> zm_inputs = {"t", "qh," "prec", "jctop", "jcbot", "pblh", "zm",
+//				"geos", "zi", "qtnd", "heat", "pap", "paph", "dpp", "delt", 
+//				"mcon", "cme", "cape", "tpert", "dlf", "pflx", "zdu", "rprd",
+//				"mu", "md", "du", "eu", "ed", "dp", "dsubcld", "jt", "maxg",
+//				"ideep", "lengath", "ql", "rliq", "landfrac", "hu_nm1",
+//      				"cnv_nm1", "tm1", "qm1", "t_star", "q_star", "dcape", "q",
+//				"tend_s", "tend_q", "cld", "snow", "ntprprd", "ntsnprd",
+//				"flxprec", "flxsnow", "ztodt", "pguall", "pgdall", "icwu",
+//				 "ncnst", "fracis"};
+//
+//
 
 const Int& lchnk = 0;
 const Int& ncol = 0;
@@ -97,89 +106,46 @@ void ZMMacrophysics::set_grids(const std::shared_ptr<const GridsManager> grids_m
   const int num_dofs = grid->get_num_local_dofs();
   const int nc = num_dofs;
 
-
-  auto VL = FieldTag::VerticalLevel;
-  auto CO = FieldTag::Column;
-  auto VR = FieldTag::Variable;
+  using namespace ShortFieldTagsNames;
+ 
+  FieldLayout scalar3d_layout_mid { {COL,VL}, {nc,NVL} }; // Note that C++ and Fortran read array dimensions in reverse
+  FieldLayout scalar3d_layout_int { {COL,VL}, {nc,NVL+1} }; // Note that C++ and Fortran read array dimensions in reverse
+  FieldLayout vector3d_layout_mid{ {COL,CMP,VL}, {nc,QSZ,NVL} };
+  FieldLayout tracers_layout { {COL,VAR,VL}, {nc,QSZ,NVL} };
   
-  FieldLayout scalar3d_layout { {CO,VL}, {nc,NVL} }; // Note that C++ and Fortran read array dimensions in reverse
-  FieldLayout vector3d_layout { {CO,VR,VL}, {nc,QSZ,NVL} };
-  FieldLayout q_forcing_layout  { {CO,VR,VL}, {nc,QSZ,NVL} };
   auto nondim = m/m;
 
-  m_required_fields.emplace("t",  vector3d_layout, Q, grid->name()); // in/out??
-  m_computed_fields.emplace("t",  vector3d_layout, Q, grid->name()); // in/out??
-
-  // We may have to init some fields from within P3. This can be the case in a P3 standalone run.
-  // Some options:
-  //  - we can tell P3 it can init all inputs or specify which ones it can init. We call the
-  //    resulting list of inputs the 'initializaable' (or initable) inputs. The default is
-  //    that no inputs can be inited.
-  //  - we can request that P3 either inits no inputs or all of the initable ones (as specified
-  //    at the previous point). The default is that P3 must be in charge of init ing ALL or NONE
-  //    of its initable inputs.
-  // Recall that:
-  //  - initable fields may not need initialization (e.g., some other atm proc that
-  //    appears earlier in the atm dag might provide them).
-
-  using strvec = std::vector<std::string>;
-//  const strvec& allowed_to_init = m_zm_params.get<strvec>("Initializable Inputs",strvec(0));
-//  const bool can_init_all = m_zm_params.get<bool>("Can Initialize All Inputs", false);
-//  const bool init_all_or_none = m_zm_params.get<bool>("Must Init All Inputs Or None", true);
-//  
-//  const strvec& initable = can_init_all ? zm_inputs : allowed_to_init;
-  if (zm_inputs.size()>0) {
-    bool all_inited = true, all_uninited = true;
-    for (const auto& name : zm_inputs) {
-      const auto& f = m_zm_fields_in.at(name);
-      const auto& track = f.get_header().get_tracking();
-      if (track.get_init_type()==InitType::None) {
-        // Nobody claimed to init this field. P3InputsInitializer will take care of it
-        m_initializer->add_me_as_initializer(f);
-        all_uninited &= true;
-        all_inited &= false;
-      } else {
-        all_uninited &= false;
-        all_inited &= true;
-      }
-    }
-
-    // In order to gurantee some consistency between inputs, it is best if P3
-    // initializes either none or all of the inputs.
-   // scream_require_msg (!init_all_or_none || all_inited || all_uninited,
-   //                     "Error! Some zm inputs were marked to be inited by P3, while others weren't.\n"
-   //                     "       P3 was requested to init either all or none of the inputs.\n");
-  }//
+  m_required_fields.emplace("t",  scalar3d_layout_int, Q, grid->name()); // in/out??
+  m_computed_fields.emplace("t",  scalar3d_layout_int, Q, grid->name()); // in/out??
 
 }
-
-//void add_field_ptr(string input){
-//  auto dev = m_zm_fields_out.at(input).get_view();
-//  auto host = Kokkos::create_mirror_view(q_dev);
-//  Kokkos::deep_copy(q_host,q_dev);
-//  auto ptr = q_host.data();
-//  return ptr
-//
-//}
-
 
 // =========================================================================================
 void ZMMacrophysics::initialize (const util::TimeStamp& t0)
 {
-  std::vector<std::string> zm_inputs = {"t"};
   
   zm_init_f90 (limcnv_in, no_deep_pbl_in);
+  m_initializer->add_me_as_initializer(f);
 
 }
-
-
 
 // =========================================================================================
 void ZMMacrophysics::run (const Real dt)
 {
+  std::vector<const Real*> in;
+  std::vector<Real*> out;
+
+  // Copy inputs to host. Copy also outputs, cause we might "update" them, rather than overwrite them.
+  for (auto& it : m_zm_fields_in) {
+    Kokkos::deep_copy(m_zm_host_views_in.at(it.first),it.second.get_view());
+  }
+  for (auto& it : m_zm_fields_out) {
+    Kokkos::deep_copy(m_zm_host_views_out.at(it.first),it.second.get_view());
+  }
+
    
-   zm_main_f90(lchnk, ncol, t, qh, prec, jctop, jcbot, pblh, zm, geos, zi, qtnd, 
-		heat, pap, paph, dpp, delt,
+   zm_main_f90(lchnk, ncol, m_raw_ptrs_out["t"], qh, prec, jctop, jcbot, 
+                pblh, zm, geos, zi, qtnd, heat, pap, paph, dpp, delt,
 		mcon, cme, cape, tpert, dlf, plfx,
 		zdu, rprd, mu, md, du, eu, ed, dp, dsubcld, jt, maxg, ideep,
 		lengath, ql, rliq, landfrac, hu_nm1, cnv_nm1,
@@ -203,16 +169,20 @@ void ZMMacrophysics::register_fields (FieldRepository<Real, device_type>& field_
  }
 
 void ZMMacrophysics::set_required_field_impl (const Field<const Real, device_type>& f) {
+  // @Meredith: Diff between add_me_as_a_customer and get_tracking().add_customer? 
+  
   // Store a copy of the field. We need this in order to do some tracking checks
   // at the beginning of the run call. Other than that, there would be really
   // no need to store a scream field here; we could simply set the view ptr
   // in the Homme's view, and be done with it.
   const auto& name = f.get_header().get_identifier().name();
   m_zm_fields_in.emplace(name,f);
+  m_zm_host_views_in[name] = Kokkos::create_mirror_view(f.get_view());
+  m_raw_ptrs_in[name] = m_zm_host_views_in[name].data();
 
   // Add myself as customer to the field
-  // @Meredith: Diff between add_me_as_a_customer and get_tracking().add_customer? 
   add_me_as_customer(f);
+
 }
 
 void ZMMacrophysics::set_computed_field_impl (const Field<      Real, device_type>& f) {
@@ -222,7 +192,10 @@ void ZMMacrophysics::set_computed_field_impl (const Field<      Real, device_typ
   // in the Homme's view, and be done with it.
   const auto& name = f.get_header().get_identifier().name();
   m_zm_fields_out.emplace(name,f);
+  m_zm_host_views_out[name] = Kokkos::create_mirror_view(f.get_view());
+  m_raw_ptrs_out[name] = m_zm_host_views_out[name].data();
+
   // Add myself as provider for the field
   add_me_as_provider(f);
-}
+  }
 } // namespace scream
---
+++ components/scream/src/physics/zm/zm_inputs_initializer.cpp
@@ -0,0 +1,206 @@
+#include "physics/zm/zm_inputs_initializer.hpp"
+#include "physics/zm/scream_zm_interface.hpp"
+
+#include <array>
+#include <string>
+namespace scream
+{
+
+
+
+std::vector<std::string> zm_inputs = {"t", "qh," "prec", "jctop", "jcbot", "pblh", "zm",
+				"geos", "zi", "qtnd", "heat", "pap", "paph", "dpp", "delt", 
+				"mcon", "cme", "cape", "tpert", "dlf", "pflx", "zdu", "rprd",
+				"mu", "md", "du", "eu", "ed", "dp", "dsubcld", "jt", "maxg",
+				"ideep", "lengath", "ql", "rliq", "landfrac", "hu_nm1",
+      				"cnv_nm1", "tm1", "qm1", "t_star", "q_star", "dcape", "q",
+				"tend_s", "tend_q", "cld", "snow", "ntprprd", "ntsnprd",
+				"flxprec", "flxsnow", "ztodt", "pguall", "pgdall", "icwu",
+				 "ncnst", "fracis"};
+
+void ZMInputsInitializer::add_field (const field_type &f)
+{
+  const auto& id = f.get_header().get_identifier();
+  
+  m_fields.emplace(id.name(),f);
+  m_fields_id.insert(id);
+}
+
+ZMInputsInitializer :: int count(){
+  int count = 0;
+  for (int i = 0 ; i < zm_inputs.size(); i++ ){
+    count += m_fields.count(zm_inputs[i]);
+  return count;
+}
+
+using namespace std;
+
+//Create struct for field info
+struct FieldInput{
+  string name;
+  Real host_mirror;
+  Real raw_ptr;
+  Real device_view;
+  Real mirror;
+  bool is_output; 
+};
+
+
+ZMInputsInitializer :: vector<FieldInput> create_fields(){
+  vector<FieldInput> fields;
+  FieldInput t;
+  FieldInput qh;
+  FieldInput prec;
+  FieldInput jctop;
+  FieldInput jcbot;
+  FieldInput pblh;
+  fields.push_back(t);
+  fields.push_back(qh);
+  fields.push_back(prec);
+  fields.push_back(jctop);
+  fields.push_back(jcbot);
+  fields.push_back(pblh);
+  return fields;
+}
+
+void do_everything(){
+  vector<FieldInput> fields = create_fields();
+  for (int i = 0; i < fields.size(); i++){
+    auto d_v = m_fields.at(fields[i].name).get_view();
+    auto p = fields[i].
+    auto m = Kokkos::create_mirror_view(fields[i].device_view);
+    fields[i].name = zm_inputs[i];
+    fields[i].device_view = d_v;
+    fields[i].mirror = m;
+    fields[i].raw_ptr = i;
+    Kokkos::deep_copy(fields[i].device_view, fields[i].raw_ptr);
+  return fields; 
+}
+
+//void assignNames(vector<FieldInput> fields){
+//  for (int i = 0; i < fields.size(); i++){
+//    fields[i].name = zm_inputs[i];
+//  }
+//}
+//
+//void get_device_views(vector<FieldInput> fields){
+//  for (int i = 0; i < fields.size(); i++){
+//    auto d_v = m_fields.at(fields[i].name.get_view());
+//    fields[i].device_view = d_v;
+//  }
+//}
+//
+//void make_mirror(vector<FieldInput> fields){
+//  for (int i = 0; i < fields.size(); i++){
+//    auto m = Kokkos::create_mirror_view(fields[i].device_view);
+//    fields[i].mirror = m;
+//  }
+//}
+//
+//// Get host mirros' raw pointers
+//void get_ptrs(vector<FieldInput> fields){
+//  for (int i = 0; i < fields.size(); i++){
+//    auto p = fields[i].
+//    fields[i].raw_ptr = i;
+//  }
+//}
+//
+//// Deep copy back to device
+//Kokkos::deep_copy(d_q,h_q);
+//void copy_to_device(vector<FieldInput> fields){
+//  for (int i = 0; i < fields.size(); i++){
+//    Kokkos::deep_copy(fields[i].device_view, fields[i].raw_ptr);
+//  }
+//}
+//
+
+
+
+//Make structs and put them in an array
+
+// =========================================================================================
+void ZMInputsInitializer::initialize_fields ()
+{
+  // Safety check: if we're askedfd to init anything at all,
+  // then we should have been asked to init 7 fields.
+  int count = 0;
+  count += m_fields.count("q");
+  count += m_fields.count("T");
+  count += m_fields.count("ast");
+  count += m_fields.count("naai");
+  count += m_fields.count("ncnuc");
+  count += m_fields.count("pmid");
+  count += m_fields.count("dp");
+  count += m_fields.count("zi");
+
+
+  for (int i = 0 ; i < zm_inputs.size(); i++ ){
+	count += m_fields.count(zm_inputs[i]);
+  }
+  std::cout << "count is " + std :: to_string(count);
+  if (count==0) {
+    return;
+  }
+
+  scream_require_msg (count==8,
+    "Error! ZMInputsInitializer is expected to init 'q','T','ast','naai','ncnuc','pmid','dp','zi'.\n"
+    "       Only " + std::to_string(count) + " of those have been found.\n"
+    "       Please, check the atmosphere processes you are using,"
+    "       and make sure they agree on who's initializing each field.\n");
+
+
+//  for (int i = 0 ; i < zm_inputs.size(); i++ ){
+//    Kokkos
+  // Get device views
+  auto d_q     = m_fields.at("q").get_view();
+  auto d_T     = m_fields.at("T").get_view();
+  auto d_ast   = m_fields.at("ast").get_view();
+  auto d_naai  = m_fields.at("naai").get_view();
+  auto d_ncnuc = m_fields.at("ncnuc").get_view();
+  auto d_pmid  = m_fields.at("pmid").get_view();
+  auto d_pdel  = m_fields.at("dp").get_view();
+  auto d_zi    = m_fields.at("zi").get_view();
+
+
+  // Create host mirrors
+  auto h_q     = Kokkos::create_mirror_view(d_q);
+  auto h_T     = Kokkos::create_mirror_view(d_T);
+  auto h_ast   = Kokkos::create_mirror_view(d_ast);
+  auto h_naai  = Kokkos::create_mirror_view(d_naai);
+  auto h_ncnuc = Kokkos::create_mirror_view(d_ncnuc);
+  auto h_pmid  = Kokkos::create_mirror_view(d_pmid);
+  auto h_pdel  = Kokkos::create_mirror_view(d_pdel); auto h_zi    = Kokkos::create_mirror_view(d_zi);
+  auto h_zi    = Kokkos::create_mirror_view(d_zi);
+
+  // Get host mirros' raw pointers
+  auto q     = h_q.data();
+  auto T     = h_T.data();
+  auto ast   = h_ast.data();
+  auto naai  = h_naai.data();
+  auto ncnuc = h_ncnuc.data();
+  auto pmid  = h_pmid.data();
+  auto pdel  = h_pdel.data();
+  auto zi    = h_zi.data();
+
+  // Call f90 routine
+//  zm_standalone_init_f90 (q, T, zi, pmid, pdel, ast, naai, ncnuc);
+  
+  // Deep copy back to device
+  Kokkos::deep_copy(d_q,h_q);
+  Kokkos::deep_copy(d_T,h_T);
+  Kokkos::deep_copy(d_ast,h_ast);
+  Kokkos::deep_copy(d_naai,h_naai);
+  Kokkos::deep_copy(d_ncnuc,h_ncnuc);
+  Kokkos::deep_copy(d_pmid,h_pmid);
+  Kokkos::deep_copy(d_pdel,h_pdel);
+  Kokkos::deep_copy(d_zi,h_zi);
+
+  // If we are in charge of init-ing FQ as well, init it to 0.
+  if (m_fields.count("FQ")==1) {
+    // Init FQ to 0
+    auto d_FQ = m_fields.at("FQ").get_view();
+    Kokkos::deep_copy(d_FQ,Real(0));
+  }
+}
+
+} // namespace scream
