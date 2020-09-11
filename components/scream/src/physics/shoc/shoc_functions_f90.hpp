0cc70d6bcc2968c0fa750aef683fb172ebe40418
Peter Andrew Bogenschutz <bogensch@quartz2300.llnl.gov>
2020-09-11 11:35:00 -0700

    update name of nlevi and add b4b hooks

--- components/scream/src/physics/shoc/shoc_functions_f90.hpp
+++ components/scream/src/physics/shoc/shoc_functions_f90.hpp
@@ -373,7 +373,8 @@ struct SHOCClipthirdmomsData : public SHOCDataBase {
   SHOCClipthirdmomsData(Int shcol_, Int nlevi_) :
     PhysicsTestData(shcol_, nlevi_,{&w_sec_zi, &w3}){}
 
-  SHOC_NO_SCALAR(SHOCClipthirdmomsData, 2);
+  PTD_STD_DEF(SHOCClipthirdmomsData, 2, 0);
+  PTD_DIM_RENAME(2, shcol, nlevi);
 
 };//SHOCClipthirdmomsData
 
