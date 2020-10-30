03b3fb2ee3b00e77077dc35b81e48bd581234053
Balwinder Singh <balwindersingh@gmail.com>
2020-10-29 23:11:54 -0700

    some fixes but code doesn't compile

---
+++ components/scream/src/physics/shoc/shoc_integ_column_stability_impl.hpp
@@ -0,0 +1,13 @@
+#ifndef SHOC_INTEG_COLUMN_STABILITY_IMPL_HPP
+#define SHOC_INTEG_COLUMN_STABILITY_IMPL_HPP
+
+#include "shoc_functions.hpp" // for ETI only but harmless for GPU
+
+namespace scream {
+  namespace shoc {
+
+
+  } // namespace shoc
+} // namespace scream
+
+#endif
