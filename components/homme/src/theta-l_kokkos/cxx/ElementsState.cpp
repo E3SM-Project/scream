2e5f407e81ee51d1d4aa6725f77e98ef7511913b
tcclevenger <tccleve@sandia.gov>
2021-07-19 13:02:56 -0600

    update

--- components/homme/src/theta-l_kokkos/cxx/ElementsState.cpp
+++ components/homme/src/theta-l_kokkos/cxx/ElementsState.cpp
@@ -331,20 +331,19 @@ void ElementsState::push_to_f90_pointers (F90Ptr& state_v, F90Ptr& state_w_i, F9
   HostViewUnmanaged<Real *[NUM_TIME_LEVELS][NUM_INTERFACE_LEV]   [NP][NP]> state_phinh_i_f90   (state_phinh_i,m_num_elems);
   HostViewUnmanaged<Real *[NUM_TIME_LEVELS][NUM_PHYSICAL_LEV ]   [NP][NP]> state_dp3d_f90      (state_dp3d,m_num_elems);
 
-  std::cout << "REF2:" << m_phinh_i(0,0,0,0,0)[0] << std::endl;
-  for (int i=0; i<m_num_elems; ++i) {
-    for (int k=0;k<NUM_TIME_LEVELS; ++k) {
-      for (int j=0; j<NUM_INTERFACE_LEV; ++j) {
-        std::cout << "REF2:" << m_phinh_i(i,k,j,0,0)[0] << std::endl;
-      }
-    }
-  }
-
   sync_to_host(m_v,         state_v_f90);
   sync_to_host(m_w_i,       state_w_i_f90);
   sync_to_host(m_vtheta_dp, state_vtheta_dp_f90);
   sync_to_host(m_phinh_i,   state_phinh_i_f90);
   sync_to_host(m_dp3d,      state_dp3d_f90);
+
+  for (int i=0; i<m_num_elems; ++i) {
+    for (int k=0;k<NUM_TIME_LEVELS; ++k) {
+      for (int j=0; j<NUM_INTERFACE_LEV; ++j) {
+        std::cout << "REF2:" << state_phinh_i_f90(i,k,j,0,0) << std::endl;
+      }
+    }
+  }
 }
 
 } // namespace Homme
