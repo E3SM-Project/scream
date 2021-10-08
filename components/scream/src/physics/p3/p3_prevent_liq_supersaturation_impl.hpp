4ea475f3e5cdabfb342d1a3bd88f2181295e3ac7
James Foucar <jgfouca@sandia.gov>
2021-10-08 16:29:51 -0600

    Need early return in cxx prevent_liq_supersaturation

--- components/scream/src/physics/p3/p3_prevent_liq_supersaturation_impl.hpp
+++ components/scream/src/physics/p3/p3_prevent_liq_supersaturation_impl.hpp
@@ -10,8 +10,8 @@ namespace p3 {
 
 //Limit sublimation and evaporation to ensure qv doesn't become supersaturated.
 //This becomes a bit subtle because of the difference between condensational
-//versus sublimational heating in the psychrometric correction. 
-  
+//versus sublimational heating in the psychrometric correction.
+
 template<typename S, typename D>
 KOKKOS_FUNCTION
 void Functions<S,D>::prevent_liq_supersaturation(const Spack& pres, const Spack& t_atm, const Spack& qv, const Spack& latent_heat_vapor, const Spack& latent_heat_sublim, const Scalar& dt, const Spack& qv2qi_vapdep_tend, const Spack& qinuc, Spack& qi2qv_sublim_tend, Spack& qr2qv_evap_tend, const Smask& context)
@@ -19,7 +19,7 @@ void Functions<S,D>::prevent_liq_supersaturation(const Spack& pres, const Spack&
 // performing calculations on.
 {
   using physics = scream::physics::Functions<Scalar, Device>;
-  
+
   constexpr Scalar inv_cp       = C::INV_CP;
   constexpr Scalar rv           = C::RV;
   constexpr Scalar qsmall       = C::QSMALL;
@@ -29,8 +29,12 @@ void Functions<S,D>::prevent_liq_supersaturation(const Spack& pres, const Spack&
   qv_sources.set(context, qi2qv_sublim_tend + qr2qv_evap_tend);
   const auto has_sources = (qv_sources>qsmall && context); //if nothing to rescale, no point in calculations.
 
+  if (not has_sources.any()) {
+    return;
+  }
+
   qv_sinks.set(has_sources, qv2qi_vapdep_tend + qinuc);
-  
+
   //Actual qv and T after microphys step
   qv_endstep.set(has_sources,qv - qv_sinks*dt + qv_sources*dt);
   T_endstep.set(has_sources,t_atm + ( (qv_sinks-qi2qv_sublim_tend)*latent_heat_sublim*inv_cp
@@ -38,7 +42,7 @@ void Functions<S,D>::prevent_liq_supersaturation(const Spack& pres, const Spack&
 
   //qv we would have at end of step if we were saturated with respect to liquid
   const auto qsl = physics::qv_sat(T_endstep,pres,false,has_sources); //"false" means NOT sat w/ respect to ice
-  
+
   //The balance we seek is:
   // qv-qv_sinks*dt+qv_sources*frac*dt=qsl+dqsl_dT*(T correction due to conservation)
   // where the T correction for conservation is:
@@ -51,21 +55,21 @@ void Functions<S,D>::prevent_liq_supersaturation(const Spack& pres, const Spack&
   // sublimation because we want to be saturated with respect to liquid at the end of the step.
   // dqsl_dt=Latent_heat_vapor*qsl/rv*T^2 following Clausius Clapeyron. Combining and solving for
   // frac yields:
-   
+
   A.set(has_sources,latent_heat_vapor*qsl*dt*inv_cp/(rv*T_endstep*T_endstep)
 	* (latent_heat_sublim*qi2qv_sublim_tend + latent_heat_vapor*qr2qv_evap_tend) );
 
-  
+
   frac.set(has_sources, (qsl-qv+qv_sinks*dt + A)/(qv_sources*dt + A) );
 
   //The only way frac<0 is if qv-qv_sinks*dt is already greater than qsl. In this case
   //the best we can do is zero out qv_sources.
   frac.set(has_sources, max(0,frac));
 
-  //The only way frac>1 is if qv-qv_sinks*dt+qv_sources*dt < qsl, in which case we shouldn't 
+  //The only way frac>1 is if qv-qv_sinks*dt+qv_sources*dt < qsl, in which case we shouldn't
   //limit anyways so set frac to 1:
   frac.set(has_sources, min(1,frac));
-  
+
   qi2qv_sublim_tend.set(has_sources, frac*qi2qv_sublim_tend );
   qr2qv_evap_tend.set(has_sources, frac*qr2qv_evap_tend);
 
