6b99e79e7d3917ecc6fc6dabefd38b6f8417d9cc
Benjamin Hillman <bhillma@sandia.gov>
2021-11-15 16:18:46 -0500

    Update YAKL and RRTMGP

--- components/scream/src/physics/rrtmgp/rrtmgp_heating_rate.hpp
+++ components/scream/src/physics/rrtmgp/rrtmgp_heating_rate.hpp
@@ -2,7 +2,7 @@
 #define RRTMGP_HEATING_RATE_HPP
 #include "physics/share/physics_constants.hpp"
 #include "YAKL/YAKL.h"
-#include "cpp/const.h"
+#include "cpp/rrtmgp_const.h"
 namespace scream {
     namespace rrtmgp {
         // Provide a routine to compute heating due to radiative fluxes. This is
