c132a3240ee2fdd510c68edfac425bdf0425b837
Luca Bertagna <lbertag@sandia.gov>
2021-11-17 11:38:37 -0700

    SCREAM: ensure correct finalization of Homme's Context singleton
    Keep track of how many objects use it, and ensure the last one
    that is destroyed also finalizes the context, regardless of the
    order in which destruction happens.

--- components/scream/src/dynamics/homme/atmosphere_dynamics.cpp
+++ components/scream/src/dynamics/homme/atmosphere_dynamics.cpp
@@ -53,7 +53,8 @@ namespace scream
 HommeDynamics::HommeDynamics (const ekat::Comm& comm, const ekat::ParameterList& params)
   : AtmosphereProcess(comm, params)
 {
-  // Nothing to do here
+  // This class needs Homme's context, so register as a user
+  HommeContextUser::singleton().add_user();
 }
 
 void HommeDynamics::set_grids (const std::shared_ptr<const GridsManager> grids_manager)
@@ -593,8 +594,10 @@ void HommeDynamics::run_impl (const int dt)
 
 void HommeDynamics::finalize_impl (/* what inputs? */)
 {
-  Homme::Context::singleton().finalize_singleton();
   prim_finalize_f90();
+
+  // This class is done needing Homme's context, so remove myself as customer
+  Homme::Context::singleton().finalize_singleton();
 }
 
 void HommeDynamics::set_computed_group_impl (const FieldGroup<Real>& group)
