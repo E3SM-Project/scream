06d21ac28037cdb2a4e807284d1ef265b8eb7b89
Oksana Guba <oksanaguba@gmail.com>
2023-01-17 18:33:22 -0500

    comments on comparing scream file with file1,file2,file3

--- cime_config/machines/cmake_macros/crayclang_crusher-gpu.cmake
+++ cime_config/machines/cmake_macros/crayclang_crusher-gpu.cmake
@@ -1,3 +1,8 @@
+#crayclang = file1
+#crayclang-crusher = file2
+#crayclanggpu-crusher = file3
+
+#present in gpu file3 and in file1, file2
 if (compile_threaded)
   string(APPEND CFLAGS " -fopenmp")
   string(APPEND FFLAGS " -fopenmp")
@@ -5,28 +10,49 @@ if (compile_threaded)
   string(APPEND LDFLAGS " -fopenmp")
 endif()
 
+#present in crayclang-crusher file2
 string(APPEND SLIBS " -L$ENV{PNETCDF_PATH}/lib -lpnetcdf")
+
+#present in file3 and in file2, why in file 3 then?
 set(NETCDF_PATH "$ENV{NETCDF_DIR}")
+#present in gpu file, same
 set(PNETCDF_PATH "$ENV{PNETCDF_DIR}")
+
+#not present anywhere, instead in gpu file3
+#Az: set(PIO_FILESYSTEM_HINTS "romio_cb_read=disable") -- 
+#whcih is already set in config_machines
 set(PIO_FILESYSTEM_HINTS "gpfs")
+#present in gpu file3 and in file2, why twice?
 string(APPEND CXX_LIBS " -lstdc++")
 
+#not present 
+#instead in files 1,2,3 vars MPICC, SCC, etc. set
 SET(CMAKE_C_COMPILER "cc" CACHE STRING "")
 SET(CMAKE_Fortran_COMPILER "ftn" CACHE STRING "")
 SET(CMAKE_CXX_COMPILER "hipcc" CACHE STRING "")
 
+#present in file3 with  --amdgpu-target=gfx90a
+#did i not use it cause kokkos puts it in?
 string(APPEND CXXFLAGS " -I${MPICH_DIR}/include")
+#present in file3 as APPEND_SLIBS
 string(APPEND LDFLAGS " -L${MPICH_DIR}/lib -lmpi -L/opt/cray/pe/mpich/8.1.16/gtl/lib -lmpi_gtl_hsa")
 
+#not present
 # For YAKL's -lroctx64 -lrocfft; the rocm module doesn't set this.
 string(APPEND LDFLAGS " -L$ENV{ROCM_PATH}/lib")
 
+# -O2 present in file3
+#-hnoacc present in file 3
+#-hfp0 present in file 3 for ELM only
+#-hipa0 present in file 3
 #this resolves a crash in mct in docn init
 if (NOT DEBUG)
   string(APPEND CFLAGS " -O2 -hnoacc -hfp0 -hipa0")
   string(APPEND FFLAGS " -O2 -hnoacc -hfp0 -hipa0")
 endif()
 
+#present in file3
 string(APPEND CPPDEFS " -DCPRCRAY")
 
+#add this to the file3
 set(SCREAM_MPI_ON_DEVICE OFF CACHE STRING "See SCREAM issue #2080.")
--- cime_config/machines/cmake_macros/crayclanggpu_crusher.cmake
+++ cime_config/machines/cmake_macros/crayclanggpu_crusher.cmake
@@ -10,17 +10,34 @@ if (DEBUG)
   string(APPEND CXXFLAGS " -O0 -g")
   string(APPEND CPPDEFS " -DYAKL_DEBUG")
 endif()
+
+#do we need -DFORTRANUNDERSCORE -DNO_R16 ?
 string(APPEND CPPDEFS " -DFORTRANUNDERSCORE -DNO_R16 -DCPRCRAY")
+
+#do we need this?
 string(APPEND FC_AUTO_R8 " -s real64")
+
+#do we need this?
 string(APPEND FFLAGS " -f free -N 255 -h byteswapio -em")
+
+#do we need this?
 if (NOT compile_threaded)
-  string(APPEND FFLAGS " -M1077")
+	string(APPEND FFLAGS " -M1077")
 endif()
+
 string(APPEND FFLAGS_NOOPT " -O0")
+
+#do we need this?
 set(HAS_F2008_CONTIGUOUS "TRUE")
+
+#do we need this?
 string(APPEND LDFLAGS " -Wl,--allow-multiple-definition")
+
+#do we need these two?
 set(SUPPORTS_CXX "TRUE")
 set(CXX_LINKER "FORTRAN")
+
+#same, why not CMAKE... 
 set(MPICC "cc")
 set(MPICXX "hipcc")
 set(MPIFC "ftn")
@@ -34,19 +51,27 @@ if (NOT DEBUG)
   string(APPEND FFLAGS   " -O2")
 endif()
 
+#this is in file2
 if (COMP_NAME STREQUAL elm)
   string(APPEND FFLAGS " -hfp0")
 endif()
+#-hipa0 -hzero are in file 2
+#do we need -hzero em ef?
 string(APPEND FFLAGS " -hipa0 -hzero -em -ef -hnoacc")
 
+#this is in file2
 set(NETCDF_PATH "$ENV{NETCDF_DIR}")
 set(PNETCDF_PATH "$ENV{PNETCDF_DIR}")
+
+#this is in config_machines
 set(PIO_FILESYSTEM_HINTS "romio_cb_read=disable")
+
 string(APPEND CMAKE_OPTS " -DPIO_ENABLE_TOOLS:BOOL=OFF")
 string(APPEND CXX_LIBS " -lstdc++")
 
 string(APPEND CXXFLAGS " -I$ENV{MPICH_DIR}/include --amdgpu-target=gfx90a")
 string(APPEND SLIBS    " -L$ENV{MPICH_DIR}/lib -lmpi -L$ENV{CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_hsa")
 
+#ZEN3? scream builds its own
 string(APPEND KOKKOS_OPTIONS " -DKokkos_ENABLE_HIP=On -DKokkos_ARCH_ZEN3=On -DKokkos_ARCH_VEGA90A=On")
 
