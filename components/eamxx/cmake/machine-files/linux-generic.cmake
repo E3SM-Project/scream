54b0ab41678e2f32ac4d4f931954e28b17be2eba
James Foucar <jgfouca@sandia.gov>
2023-02-23 15:10:02 -0700

    Update EKAT, use new test-launcher setting in linux-generic machines

--- components/eamxx/cmake/machine-files/linux-generic.cmake
+++ components/eamxx/cmake/machine-files/linux-generic.cmake
@@ -1,3 +1,6 @@
 # Load all kokkos settings from Ekat's mach file
 set (EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/cmake/machine-files)
 include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
+
+# Remove this if you are using a resource manager (slurm etc)
+set (EKAT_TEST_LAUNCHER_MANAGE_RESOURCES True CACHE BOOL "")
