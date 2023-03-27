75b15bcecff972d971d06c19fdfb2939cd3cbd6b
Trey White <trey.white@hpe.com>
2023-03-27 19:36:08 -0400

    Added optimized "caar compute" implementation.

--- cime_config/machines/cmake_macros/crayclang-scream_crusher-scream-gpu.cmake
+++ cime_config/machines/cmake_macros/crayclang-scream_crusher-scream-gpu.cmake
@@ -16,7 +16,7 @@ SET(CMAKE_Fortran_COMPILER "ftn" CACHE STRING "")
 SET(CMAKE_CXX_COMPILER "hipcc" CACHE STRING "")
 
 string(APPEND CXXFLAGS " -I${MPICH_DIR}/include")
-string(APPEND LDFLAGS " -L${MPICH_DIR}/lib -lmpi -L/opt/cray/pe/mpich/8.1.16/gtl/lib -lmpi_gtl_hsa")
+string(APPEND LDFLAGS " -L${MPICH_DIR}/lib -lmpi ${PE_MPICH_GTL_DIR_amd_gfx90a} ${PE_MPICH_GTL_LIBS_amd_gfx90a}")
 
 # For YAKL's -lroctx64 -lrocfft; the rocm module doesn't set this.
 string(APPEND LDFLAGS " -L$ENV{ROCM_PATH}/lib")
