9c508e5fe6fc092b54a5b54dc325028018876fe4
Elynn Wu <elynn510@gmail.com>
2023-06-05 14:12:29 -0700

    add pnetcdf on ruby

--- components/eamxx/scripts/machines_specs.py
+++ components/eamxx/scripts/machines_specs.py
@@ -34,7 +34,7 @@ MACHINE_METADATA = {
                  ["mpicxx","mpifort","mpicc"],
                   "bsub -Ip -qpdebug",
                   ""),
-    "ruby-intel" : (["module --force purge", "module load StdEnv cmake/3.19.2 mkl/2022.1.0 intel-classic/2021.6.0-magic netcdf-c-parallel/4.9.0 netcdf-fortran-parallel/4.6.0 mvapich2/2.3.7 python/3.9.12"],
+    "ruby-intel" : (["module --force purge", "module use --append /usr/gdata/climdat/install/quartz/modulefiles", "module load StdEnv cmake/3.19.2 mkl/2022.1.0 intel-classic/2021.6.0-magic netcdf-c-parallel/4.9.0 netcdf-fortran-parallel/4.6.0 pnetcdf/1.12.3 mvapich2/2.3.7 python/3.9.12"],
                  ["mpicxx","mpifort","mpicc"],
                   "salloc --partition=pdebug",
                   ""),
