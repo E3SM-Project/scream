8dcea1c90f1f775e0e9b75c5f4d2f6b64e6e8a0d
Sarat Sreepathi <sarat@ornl.gov>
2023-07-29 01:08:01 -0400

    Backport Frontier machine config to old branch

--- cime_config/machines/cmake_macros/crayclang-scream_frontier-scream-gpu.cmake
+++ cime_config/machines/cmake_macros/crayclang-scream_frontier-scream-gpu.cmake
@@ -1,5 +1,17 @@
+# set(MPICC "cc")
+# set(MPICXX "hipcc")
+# set(MPIFC "ftn")
+# set(SCC "cc")
+# set(SCXX "hipcc")
+# set(SFC "ftn")
+# 
+string(APPEND CPPDEFS " -DLINUX")
+if (COMP_NAME STREQUAL gptl)
+    string(APPEND CPPDEFS " -DHAVE_NANOTIME -DBIT64 -DHAVE_SLASHPROC -DHAVE_COMM_F2C -DHAVE_TIMES -DHAVE_GETTIMEOFDAY")
+endif()
+
 if (compile_threaded)
-  #string(APPEND CFLAGS " -fopenmp")
+  string(APPEND CFLAGS " -fopenmp")
   string(APPEND FFLAGS " -fopenmp")
   string(APPEND CXXFLAGS " -fopenmp")
   string(APPEND LDFLAGS " -fopenmp")
@@ -8,10 +20,12 @@ endif()
 string(APPEND SLIBS " -L$ENV{PNETCDF_PATH}/lib -lpnetcdf")
 set(NETCDF_PATH "$ENV{NETCDF_DIR}")
 set(PNETCDF_PATH "$ENV{PNETCDF_DIR}")
-set(PIO_FILESYSTEM_HINTS "gpfs")
 string(APPEND CXX_LIBS " -lstdc++")
 
-SET(CMAKE_C_COMPILER "cc" CACHE STRING "")
+string(APPEND SLIBS " -L$ENV{ROCM_PATH}/lib ")
+string(APPEND FFLAGS " -hipa0 -hzero -f free")
+
+SET(CMAKE_C_COMPILER "mpicc" CACHE STRING "")
 SET(CMAKE_Fortran_COMPILER "ftn" CACHE STRING "")
 SET(CMAKE_CXX_COMPILER "hipcc" CACHE STRING "")
 
@@ -20,8 +34,9 @@ string(APPEND CXXFLAGS " -I$ENV{ROCM_PATH}/include")
 
 # Crusher: this resolves a crash in mct in docn init
 if (NOT DEBUG)
-  string(APPEND CFLAGS " -O2 -hnoacc -hfp0 -hipa0")
-  string(APPEND FFLAGS " -O2 -hnoacc -hfp0 -hipa0")
+  string(APPEND CFLAGS " -O3 -hnoacc -hfp0 -hipa0")
+  string(APPEND FFLAGS " -O3 -hnoacc -hfp0 -hipa0")
+  string(APPEND CXXFLAGS " -O3 ")
 endif()
 
 string(APPEND CPPDEFS " -DCPRCRAY")
