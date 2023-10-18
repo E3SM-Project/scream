0b3faaf0924cd03d447fa5a68ca54226dc0cb303
Balwinder Singh <balwindersingh@gmail.com>
2023-10-17 18:10:35 -0700

    Remove all code from nuc ice

--- components/eam/src/physics/cam/microp_aero.F90
+++ components/eam/src/physics/cam/microp_aero.F90
@@ -1,78 +1,23 @@
 
 module microp_aero
 
-!---------------------------------------------------------------------------------
-! Purpose:
-!   CAM driver layer for aerosol activation processes.
-!
-! ***N.B.*** This module is currently hardcoded to recognize only the aerosols/modes that
-!            affect the climate calculation.  This is implemented by using list
-!            index 0 in all the calls to rad_constituent interfaces.
-!
-! Author: Andrew Gettelman
-! Based on code from: Hugh Morrison, Xiaohong Liu and Steve Ghan
-! May 2010
-! Description in: Morrison and Gettelman, 2008. J. Climate (MG2008)
-!                 Gettelman et al., 2010 J. Geophys. Res. - Atmospheres (G2010)         
-! for questions contact Andrew Gettelman  (andrew@ucar.edu)
-! Modifications: A. Gettelman Nov 2010  - changed to support separation of 
-!                  microphysics and macrophysics and concentrate aerosol information here
-!                B. Eaton, Sep 2014 - Refactored to move CAM interface code into the CAM
-!                  interface modules and preserve just the driver layer functionality here.
-!
-!---------------------------------------------------------------------------------
-
-use shr_kind_mod,     only: r8=>shr_kind_r8
-use spmd_utils,       only: masterproc
-use ppgrid,           only: pcols, pver, pverp
-use ref_pres,         only: top_lev => trop_cloud_top_lev
-use physconst,        only: rair, gravit, pi
-use constituents,     only: cnst_get_ind
-use physics_types,    only: physics_state, physics_ptend, physics_ptend_init
-use physics_buffer,   only: physics_buffer_desc, pbuf_get_index, pbuf_old_tim_idx, pbuf_get_field
-use phys_control,     only: phys_getopts, cam_chempkg_is, use_hetfrz_classnuc
-use rad_constituents, only: rad_cnst_get_info, rad_cnst_get_aer_mmr, rad_cnst_get_aer_props, &
-                            rad_cnst_get_mode_num
-
-use nucleate_ice_cam
-
-use ndrop,            only: ndrop_init, dropmixnuc
-use ndrop_bam,        only: ndrop_bam_init, ndrop_bam_run, ndrop_bam_ccn
-
-
-use cam_history,      only: addfld, add_default, outfld
-use cam_logfile,      only: iulog
-use cam_abortutils,       only: endrun
-use perf_mod,         only: t_startf, t_stopf
+use ppgrid,           only: pver
 
 implicit none
 private
 save
 
 public ::  microp_aero_register
-
-! Private module data
-
 integer :: npccn_idx
+integer :: naai_idx
 
 contains
-!=========================================================================================
-
 subroutine microp_aero_register
-   !----------------------------------------------------------------------- 
-   ! 
-   ! Purpose: 
-   ! Register pbuf fields for aerosols needed by microphysics
-   ! 
-   ! Author: Cheryl Craig October 2012
-   ! 
-   !-----------------------------------------------------------------------
    use ppgrid,         only: pcols
    use physics_buffer, only: pbuf_add_field, dtype_r8
 
    call pbuf_add_field('NPCCN',      'physpkg',dtype_r8,(/pcols,pver/), npccn_idx)
-   call nucleate_ice_cam_register()
-
+   call pbuf_add_field('NAAI',     'physpkg', dtype_r8, (/pcols,pver/), naai_idx)
 end subroutine microp_aero_register
 
 end module microp_aero
--- components/eam/src/physics/cam/nucleate_ice_cam.F90
+++ components/eam/src/physics/cam/nucleate_ice_cam.F90
@@ -7,41 +7,16 @@ module nucleate_ice_cam
 !  B. Eaton - Sept 2014
 !---------------------------------------------------------------------------------
 
-use shr_kind_mod,   only: r8=>shr_kind_r8
-use spmd_utils,     only: masterproc
+
 use ppgrid,         only: pcols, pver
-use physconst,      only: pi, rair, tmelt
-use constituents,   only: cnst_get_ind
-use physics_types,  only: physics_state
-use physics_buffer, only: physics_buffer_desc, pbuf_get_index, pbuf_old_tim_idx, pbuf_get_field
-use phys_control,   only: use_hetfrz_classnuc
-use rad_constituents, only: rad_cnst_get_info, rad_cnst_get_aer_mmr, rad_cnst_get_aer_props, &
-                            rad_cnst_get_mode_num, rad_cnst_get_mode_props
-
-use physics_buffer, only: pbuf_add_field, dtype_r8, pbuf_old_tim_idx, &
-                          pbuf_get_index, pbuf_get_field
-use cam_history,    only: addfld, add_default, outfld
-
-use ref_pres,       only: top_lev => trop_cloud_top_lev
-use wv_saturation,  only: qsat_water
-#ifndef HAVE_ERF_INTRINSICS
-use shr_spfn_mod,   only: erf => shr_spfn_erf
-#endif
-use cam_logfile,    only: iulog
-use cam_abortutils, only: endrun
-
-use nucleate_ice,   only: nucleati_init, nucleati
+use physics_buffer, only: pbuf_add_field, dtype_r8
 
 
 implicit none
 private
-save
-
 public :: nucleate_ice_cam_register
 
-integer :: &
-   naai_idx,     &
-   naai_hom_idx
+integer :: naai_idx
 
 !===============================================================================
 contains
@@ -49,8 +24,7 @@ contains
 
 subroutine nucleate_ice_cam_register()
 
-   call pbuf_add_field('NAAI',     'physpkg', dtype_r8, (/pcols,pver/), naai_idx)
-   call pbuf_add_field('NAAI_HOM', 'physpkg', dtype_r8, (/pcols,pver/), naai_hom_idx)
+
 
 end subroutine nucleate_ice_cam_register
 
