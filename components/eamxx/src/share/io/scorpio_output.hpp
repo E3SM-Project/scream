138682441197229b6b9bf9e53f5f7974b427ff8e
Luca Bertagna <lbertag@sandia.gov>
2024-01-23 15:26:29 -0700

    EAMxx: remove unused member in scorpio output

--- components/eamxx/src/share/io/scorpio_output.hpp
+++ components/eamxx/src/share/io/scorpio_output.hpp
@@ -200,7 +200,6 @@ protected:
   std::map<std::string,std::string>                     m_field_to_avg_cnt_map;
   std::map<std::string,std::string>                     m_field_to_avg_cnt_suffix;
   std::map<std::string,FieldLayout>                     m_layouts;
-  std::map<std::string,int>                             m_dofs;
   std::map<std::string,int>                             m_dims;
   std::map<std::string,std::shared_ptr<atm_diag_type>>  m_diagnostics;
   std::map<std::string,std::vector<std::string>>        m_diag_depends_on_diags;
