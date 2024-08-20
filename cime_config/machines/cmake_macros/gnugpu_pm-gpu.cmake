916cf09fe15d8b01a6a4f66ff592b5e168b0e0d4
noel <ndkeen@lbl.gov>
2024-08-20 07:04:24 -0700

    profiling

--- cime_config/machines/cmake_macros/gnugpu_pm-gpu.cmake
+++ cime_config/machines/cmake_macros/gnugpu_pm-gpu.cmake
@@ -5,7 +5,8 @@ if (COMP_NAME STREQUAL gptl)
   string(APPEND CPPDEFS " -DHAVE_NANOTIME -DBIT64 -DHAVE_SLASHPROC -DHAVE_GETTIMEOFDAY")
 endif()
 string(APPEND CPPDEFS " -DTHRUST_IGNORE_CUB_VERSION_CHECK")
-string(APPEND CMAKE_CUDA_FLAGS " -ccbin CC -O2 -arch sm_80 --use_fast_math")
+#string(APPEND CMAKE_CUDA_FLAGS " -ccbin CC -O2 -arch sm_80 --use_fast_math")
+string(APPEND CMAKE_CUDA_FLAGS " -ccbin CC -O2 -arch sm_80 --use_fast_math -lineinfo") # ndk, only beneficial when using ncu profiling
 string(APPEND KOKKOS_OPTIONS " -DKokkos_ARCH_AMPERE80=On -DKokkos_ENABLE_CUDA=On -DKokkos_ENABLE_CUDA_LAMBDA=On -DKokkos_ENABLE_SERIAL=ON -DKokkos_ENABLE_OPENMP=Off -DKokkos_ENABLE_IMPL_CUDA_MALLOC_ASYNC=Off")
 set(CMAKE_CUDA_ARCHITECTURES "80")
 #string(APPEND CMAKE_CXX_FLAGS_RELEASE " -g -DNSIGHT")
@@ -19,5 +20,13 @@ set(SCC "cc")
 set(SCXX "CC")
 set(SFC "ftn")
 
+# ndk needed this at one point, but not sure we still do. needs to only be at final link, hence only for comp_name==cpl
+if (COMP_NAME STREQUAL cpl)
+  string(APPEND CMAKE_EXE_LINKER_FLAGS " /opt/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/12.2/targets/x86_64-linux/lib/libnvToolsExt.so")
+endif()
+
+# experimenting with YAKL flags
 #string(APPEND CPPDEFS  " -DYAKL_AUTO_PROFILE -DYAKL_PROFILE")
-#set(SCREAM_ENABLE_MAM OFF) # ndk
+
+# don't actually need MAM, plus it auto adds openmp flags which we do not want
+set(SCREAM_ENABLE_MAM OFF) # ndk
