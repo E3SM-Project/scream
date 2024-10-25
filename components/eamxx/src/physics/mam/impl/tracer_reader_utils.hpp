6348f1e092af49e30f82dacfd0e605e13514d278
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2024-10-25 10:45:37 -0600

    Refactoring: Renamed compute_days function for better clarity and added detailed comments.

--- components/eamxx/src/physics/mam/impl/tracer_reader_utils.hpp
+++ components/eamxx/src/physics/mam/impl/tracer_reader_utils.hpp
@@ -198,9 +198,12 @@ inline util::TimeStamp convert_date(const int date) {
   int day   = date - year * ten_thousand - month * one_hundred;
   return util::TimeStamp(year, month, day, 0, 0, 0);
 }
-// FIXME: check if this function is implemented in eamxx
-// Assumes 365 days/year, 30 days/month
-inline int compute_days(const util::TimeStamp &ts) {
+// FIXME: This function is not implemented in eamxx.
+// FIXME: Assumes 365 days/year, 30 days/month;
+// NOTE: that this assumption is mainly used for plotting.
+// NOTE: This is not a direct port from EAM.
+//We only use this routine for chlorine.
+inline int compute_number_days_from_zero(const util::TimeStamp &ts) {
   return ts.get_year() * 365 + ts.get_month() * 30 + ts.get_day();
 }
 
@@ -211,7 +214,7 @@ inline void create_linoz_chlorine_reader(
   auto time_stamp_beg = convert_date(chlorine_loading_ymd);
 
   const int offset_time =
-      compute_days(time_stamp_beg) - compute_days(model_time);
+      compute_number_days_from_zero(time_stamp_beg) - compute_number_days_from_zero(model_time);
   scorpio::register_file(linoz_chlorine_file, scorpio::Read);
   const int nlevs_time = scorpio::get_time_len(linoz_chlorine_file);
   for(int itime = 0; itime < nlevs_time; ++itime) {
@@ -222,7 +225,7 @@ inline void create_linoz_chlorine_reader(
       scorpio::read_var(linoz_chlorine_file, "chlorine_loading", &value, itime);
       values.push_back(value);
       auto time_stamp = convert_date(date);
-      time_secs.push_back(compute_days(time_stamp) - offset_time);
+      time_secs.push_back(compute_number_days_from_zero(time_stamp) - offset_time);
     }
   }  // end itime
   scorpio::release_file(linoz_chlorine_file);
@@ -263,7 +266,7 @@ inline void get_time_from_ncfile(const std::string &file_name,
 inline Real chlorine_loading_advance(const util::TimeStamp &ts,
                                      std::vector<Real> &values,
                                      std::vector<int> &time_secs) {
-  const int current_time = compute_days(ts);
+  const int current_time = compute_number_days_from_zero(ts);
   int index              = 0;
   // update index
   for(int i = 0; i < int(values.size()); i++) {
